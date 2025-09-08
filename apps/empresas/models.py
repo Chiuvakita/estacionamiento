3744a6496cb7908a741678562fa324b39cdcf3b6
AUDIBIX <cbbenja@hotmail.com>
2025-09-08 15:56:18 -0300

    Se definen modelos
    Se estipularon los modelos y su relación según la base de datos sql

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+#Crear modelo de django para almacenar la información
+#No se agrega id porque django ya trabaja con sus propios ids autoincrementales
+#No se agrega encapsulamiento ya que django necesita acceder a los datos de forma simple
+
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=300)
+    telefono = models.CharField(max_length=45)
+    correo = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=300)
+    
+class Sucursal(models.Model):
+    cantidadEstacionamiento = models.IntegerField()
+    nombreSucursal = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=300)
+    numero = models.CharField(max_length=45)
+    empresa = models.ForeignKey( #Esto es la foreign key, relaciona ambos modelos
+        Empresa, #Esta es la referencia, a qué modelo apunta
+        on_delete=models.CASCADE, #Cascade indica que se borran las sucursales si se borra la empresa
+        related_name="sucursales" #Esto es para acceder 
+    )
--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def listar_empresas(request):
     return render(request, "empresaListar.html")
-
+    
 
 # Realizar listarEmpresa
 # Realizar crearEmpresa
