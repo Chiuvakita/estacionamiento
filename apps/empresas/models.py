f61e8e92de5a5581c5ec51d015610d031365c4e2
AUDIBIX <cbbenja@hotmail.com>
2025-11-17 11:44:27 -0300

    Str
    Se agregó la función str a las clases

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -10,6 +10,10 @@ class Empresa(models.Model):
     telefono = models.CharField(max_length=45)
     correo = models.EmailField(max_length=254)
     direccion = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f" Empresa: {self.nombre} \n Telefono: {self.telefono} \n Correo: {self.correo} \n Direccion: {self.direccion}"
+
     
 class Sucursal(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, default=None)
@@ -17,3 +21,6 @@ class Sucursal(models.Model):
     nombreSucursal = models.CharField(max_length=100)
     direccion = models.CharField(max_length=300)
     numero = models.CharField(max_length=45)
+
+    def __str__(self):
+        return f" Empresa: {self.empresa} \n Sucursal: {self.nombreSucursal} \n Direccion: {self.direccion} \n Numero: {self.numero} \n Cantidad de estacionamiento: {self.cantidadEstacionamiento}"
